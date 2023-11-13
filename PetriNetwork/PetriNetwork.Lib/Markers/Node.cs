01163cc79f63b934243f64f5af907ab34acec4be
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-13 20:10:08 +0200

    add node

---
+++ PetriNetwork/PetriNetwork.Lib/Markers/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace PetriNetwork.Lib.Markers;
+
+public class Node
+{
+    private double _prevTimeUpdate = 0;
+    public double TimeInSystem { get; set; }
+    public double RepairTime { get; set; }
+    public bool PartiallyProcessed { get; set; }
+    
+    public void Update(double currTime)
+    {
+        TimeInSystem += currTime - _prevTimeUpdate;
+    }
+}
