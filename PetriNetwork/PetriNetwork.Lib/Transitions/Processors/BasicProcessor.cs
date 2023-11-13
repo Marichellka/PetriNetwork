be70339ab781f106d885e95f4ff725dda1e36a12
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-13 20:10:00 +0200

    add basic processor

---
+++ PetriNetwork/PetriNetwork.Lib/Transitions/Processors/BasicProcessor.cs
@@ -0,0 +1,30 @@
+﻿namespace PetriNetwork.Lib.Transitions.Processors;
+
+public class BasicProcessor: IProcessor
+{
+    public PriorityQueue<IEnumerable<object>, double> ProcessingItems { get; }
+    public double NextEventTime
+    {
+        get
+        {
+            if (ProcessingItems.TryPeek(out _, out double priority))
+                return priority;
+            return Double.MaxValue;
+        }
+    }
+
+    public BasicProcessor()
+    {
+        ProcessingItems = new PriorityQueue<IEnumerable<object>, double>();
+    }   
+    
+    public void Process(IEnumerable<object> markers, double timeCompletion)
+    {
+        ProcessingItems.Enqueue(markers, timeCompletion);
+    }
+
+    public IEnumerable<object> EndProcess()
+    {
+        return ProcessingItems.Dequeue();
+    }
+}
---
+++ PetriNetwork/PetriNetwork.Lib/Transitions/Processors/IProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace PetriNetwork.Lib.Transitions.Processors;
+
+public interface IProcessor
+{
+    public PriorityQueue<IEnumerable<object>, double> ProcessingItems { get; }
+    public double NextEventTime { get; } 
+
+    public void Process(IEnumerable<object> markers, double timeCompletion);
+
+    public IEnumerable<object> EndProcess();
+}
