fa06d95cc7b434cd3ee6a1f995b21ea3fefcd018
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-16 22:59:03 +0200

    add basic queue

---
+++ PetriNetwork/PetriNetwork.Lib/Markers/Queues/Queue.cs
@@ -0,0 +1,32 @@
+﻿namespace PetriNetwork.Lib.Markers.Queues;
+
+public class Queue<TItem>: IQueue<TItem>
+{
+    private System.Collections.Generic.Queue<TItem> _queue;
+    public int Count { get=>_queue.Count; }
+
+    public Queue()
+    {
+        _queue = new();
+    }
+
+    public Queue(IEnumerable<TItem> items)
+    {
+        _queue = new(items);
+    }
+
+    public TItem Peek()
+    {
+        return _queue.Peek();
+    }
+
+    public void Enqueue(TItem item)
+    {
+        _queue.Enqueue(item);
+    }
+    
+    public TItem Dequeue()
+    {
+        return _queue.Dequeue();
+    }
+}
