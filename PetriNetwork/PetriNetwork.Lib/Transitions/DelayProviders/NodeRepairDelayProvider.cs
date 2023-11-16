d59fe5d7c2103de0c4678b5689ce7a537c7d827f
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-16 16:14:02 +0200

    add specific delay provider for repair station

---
+++ PetriNetwork/PetriNetwork.Lib/Transitions/DelayProviders/NodeRepairDelayProvider.cs
@@ -0,0 +1,24 @@
+﻿using PetriNetwork.Lib.Markers;
+
+namespace PetriNetwork.Lib.Transitions.DelayProviders;
+
+public class NodeRepairDelayProvider: IDelayProvider
+{
+    public double GetDelay(IEnumerable<object> items)
+    {
+        double delay =- 1;
+        foreach (var item in items)
+        {
+            if (item is Node node)
+            {
+                if (Math.Abs(delay - (-1)) > 0.0001)
+                    throw new ArgumentException("Provided more than 1 Node");
+
+                delay = node.RepairTime;
+            }
+                
+        }
+
+        return delay;
+    }
+}
