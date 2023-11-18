64e1c4a0afcb61e0586c38adf8ffb89e587df918
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-18 11:40:01 +0200

    finish network simulation flow

--- PetriNetwork/PetriNetwork.Lib/Network/Network.cs
+++ PetriNetwork/PetriNetwork.Lib/Network/Network.cs
@@ -8,15 +8,15 @@ namespace PetriNetwork.Lib.Network;
 public class Network
 {
     public List<Transition> Transitions { get; }
-    public List<Position<object>> Positions { get; }
-    public List<Arc<object>> Arcs { get; }
+    public List<Position> Positions { get; }
+    public List<Arc> Arcs { get; }
     private IConflictResolver _conflictResolver;
     private double _currTime = 0;
     private double _nextTime = double.MaxValue;
 
     public Network(
-        List<Transition> transitions, List<Position<object>> positions, 
-        List<Arc<object>> arcs, IConflictResolver conflictResolver)
+        List<Transition> transitions, List<Position> positions, 
+        List<Arc> arcs, IConflictResolver conflictResolver)
     {
         Transitions = transitions;
         Positions = positions;
@@ -56,9 +56,16 @@ public class Network
     {
         var activeTransitions = GetActiveTransitions();
         _nextTime = double.MaxValue;
+
         while (activeTransitions.Count > 0)
         {
-            var transition = _conflictResolver.ResolveConflict(activeTransitions);
+            var transition = activeTransitions[0];
+            var conflictFinder = new ConflictFinder();
+            conflictFinder.TryFindConflict(transition);
+            if (conflictFinder.Transitions.Count != 0) 
+                transition = _conflictResolver.ResolveConflict(
+                    conflictFinder.Transitions.ToList(), conflictFinder.Positions.ToList());
+            
             transition.CurrTime = _currTime;
             transition.StartTransition();
             _nextTime = Math.Min(_nextTime, transition.NextEventTime);
@@ -71,7 +78,7 @@ public class Network
         List<Transition> activeTransitions = new List<Transition>();
         foreach (var transition in Transitions)
         {
-            if (transition.IsConditionsDone())
+            if (transition.IsReady())
             {
                 activeTransitions.Add(transition);
             }
@@ -84,7 +91,7 @@ public class Network
     {
         foreach (var transition in Transitions)
         {
-            if (transition.NextEventTime == _currTime)
+            while (Math.Abs(transition.NextEventTime - _currTime) < 0.0001)
             {
                 transition.EndTransition();
                 transition.CurrTime = _currTime;
