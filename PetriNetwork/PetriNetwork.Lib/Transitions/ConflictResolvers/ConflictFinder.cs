fda431bcfddf8fe1a511e0c2c3b5cca7fc249dc3
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-18 11:38:10 +0200

    add conflict finder

---
+++ PetriNetwork/PetriNetwork.Lib/Transitions/ConflictResolvers/ConflictFinder.cs
@@ -0,0 +1,28 @@
+﻿using PetriNetwork.Lib.Positions;
+
+namespace PetriNetwork.Lib.Transitions.ConflictResolvers;
+
+public class ConflictFinder
+{
+    public HashSet<Transition> Transitions { get; } = new HashSet<Transition>();
+    public HashSet<Position> Positions { get; } = new HashSet<Position>();
+
+    public void TryFindConflict(Transition transition)
+    {
+        if (Transitions.Contains(transition) || !transition.IsReady())
+            return;
+
+        Transitions.Add(transition);
+        foreach (var position in transition.GetInPositions())
+        {
+            if (!Positions.Contains(position))
+            {
+                Positions.Add(position);
+                foreach (var transition1 in position.GetOutTransitions())
+                {
+                    TryFindConflict(transition1);
+                }
+            }
+        }
+    }
+}
